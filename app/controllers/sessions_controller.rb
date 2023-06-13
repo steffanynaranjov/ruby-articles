fe86a3408c8c60622eedd479fd0604fa90a9df25
Steffany Naranjo V <steffany.naranjo@gmail.com>
2023-06-13 17:29:24 -0500

    update code

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You are now authenticated"
+      redirect_to user
+    else
+      flash.now[:alert] = "your login credentials are wrong"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+end
